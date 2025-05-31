758eba5110db31b74d8ab08f84d22c324e5d8e77
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-05-31 19:29:23 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -50,7 +50,7 @@ st.pyplot(fig)
 # st.title('Implied Volatility Surface')
 #st.title('Time Fractional Black - Scholes Partial Differential Equation (tfBSPDE)')
 
-data = pd.read_csv('/Users/sylvestercleveland/Desktop/Closed Loop Forecast Data.csv')
+data = pd.read_csv('Closed Loop Forecast Data.csv')
 data = pd.DataFrame(data)
 fig, ax = plt.subplots(3, 1, figsize=(12, 10))
 #for i in range(0, data.columns.size):
