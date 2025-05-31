a7a51fab17ee9f908b1fd082557e7a96b4ca361f
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-05-31 17:43:17 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -17,7 +17,7 @@ from scipy.optimize import brentq
 from scipy.interpolate import griddata
 import plotly.graph_objects as go
 #import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 %config InlineBackend.figure_formats='svg'
 
 x = np.linspace(-20, 20, 256)
