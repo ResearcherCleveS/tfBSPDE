b780d81bba19d9f30b8cc294d57e5c340a0b751d
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-01 19:29:04 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -65,7 +65,7 @@ for i in range(0, len(data.columns)):
         datalist[i].append(float(j))
     datalist[i] = pd.DataFrame({f"Channel_{i+1}": datalist[i]})
     ax[i].plot(datalist[i]);
-    ax[i].set_title(f"Channel {i}")
+    ax[i].set_title(f"Channel {i+1}")
 st.pyplot(fig)
 df = np.zeros((len(datalist[0]), 3))
 for i in range(0, 3):
