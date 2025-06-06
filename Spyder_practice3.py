974353664194b81ba00a99aa4ffcac7c8e56ccfd
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-05 23:05:42 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 #%config InlineBackend.figure_formats='svg'
 
 st.title("Author Sylvester Cleve & Hinton's the Greatest❣️💠") 
-#st.subheader("**& Hinton's the Greatest❣️💠**") 
+
 
 x = np.linspace(-20, 20, 256)
 f = np.sin(x)/(1e-8+x)
