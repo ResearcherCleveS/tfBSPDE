689525146c79f3c4a10b0fa1dc648acff9e84bce
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-06 14:43:27 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 
 st.title("Author Sylvester Cleve")
 st.header("Hinton & ResearcherCleveS are Successes!")
-st.subheader("KC Special Member♥️💠")
+st.subheader("Hinton & ResearcherCleveS, KC Special Member♥️💠 are Successes!")
 with st.sidebar:
     st.title("**AI plus Partial Differential Eqn ie Physics Informed Neural Network (PINN) demonstration.**")
     st.write("**We'll explore implementing a PINN to optimize the efficiency and application of the Black Scholes Formula.**")
