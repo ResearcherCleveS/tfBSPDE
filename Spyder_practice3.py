9de74cc5a52d110fcd7b35b670bccc20be820a47
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-05 22:47:14 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -20,7 +20,8 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 #%config InlineBackend.figure_formats='svg'
 
-st.title("Author Sylvester Cleve and Hinton's the Greatest Ever do it!!")
+st.title("Author Sylvester Cleve") 
+st.subheader("And Hinton's the Greatest Ever do it!!")
 
 x = np.linspace(-20, 20, 256)
 f = np.sin(x)/(1e-8+x)
