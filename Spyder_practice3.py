a385d20ad46d5e697d1e258d8e9a4cef75317c3f
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-06 13:43:31 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -20,8 +20,8 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 #%config InlineBackend.figure_formats='svg'
 
-st.title("Author Sylvester Cleve") 
-st.success("Hinton & ResearcherCleveS are Successes! On Rudy Ru")
+st.title("Author Sylvester Cleve On Rudy Ru") 
+st.success("Hinton & ResearcherCleveS are Successes!")
 
 x = np.linspace(-20, 20, 256)
 f = np.sin(x)/(1e-8+x)
