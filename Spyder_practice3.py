a509e01a5db2d1073f47149fe36ecf487ef54dcb
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-06 20:31:27 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -20,8 +20,9 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 #%config InlineBackend.figure_formats='svg'
 
+from langchain import llms
+from langchain import agents #creat_pandas_dataframe_agent
 from langchain_community.llms import OpenAI
-from langchain_community.agents import creat_pandas_dataframe_agent
 from dotenv import load_dotenv, find_dotenv 
 
 st.title("Author ResearcherCleveS 👑")
