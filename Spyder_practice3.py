ceaa6b52646fb45fc8eebad7c6e92839ed994bfb
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-06-06 21:09:48 -0400

    Update Spyder_practice3.py

--- Spyder_practice3.py
+++ Spyder_practice3.py
@@ -27,7 +27,7 @@ from langchain.llms import OpenAI
 from langchain import agents #creat_pandas_dataframe_agent
 from dotenv import load_dotenv, find_dotenv 
 
-os.environ["OPEN_API_KEY"] = apikey
+os.environ['OPEN_API_KEY'] = apikey
 load_dotenv(find_dotenv())
 
 # llm model
